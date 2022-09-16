276d8881f105ac014271720c94ae103ffc94d154
true-dude <79796550+true-dude@users.noreply.github.com>
2022-09-16 10:18:04 +0300

    v3

--- 1/input.cpp
+++ 1/input.cpp
@@ -3,11 +3,13 @@
 
 int preparation(float* resistance)
 {
+    assert(resistance != NULL);
+
     int number_elems = 0;
 
     float voltage = NAN;
     float current = NAN;
-    while (scanf("%f%*c%f", &voltage, &current) && number_elems < MAX_SIZE)
+    while (scanf("%f%*c%f", &voltage, &current) && number_elems < MAX_SIZE && resistance != NULL)
     {
         *resistance = voltage / current;
         resistance++;
