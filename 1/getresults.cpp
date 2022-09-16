0ad783a57d2e3eae3d3da68b06a2cb9697b2773c
Radamoviches <55924626+Radamoviches@users.noreply.github.com>
2022-09-16 10:26:41 +0300

    Merge branch 'main' of https://github.com/karetskiiVO/labs

---
+++ 1/getresults.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "getresults.h"
+#include <math.h>
+
+float results(int K, float *resistance){
+    float resistance_final = 0 ;
+    float deviation = 0;
+    for (int i = 0; i < K ;i++){
+        resistance_final += resistance[i];
+    }
+    resistance_final /= K;
+    for (int i = 0; i < K; i++){
+        deviation += (resistance_final - resistance[i]) * (resistance_final - resistance[i]);
+    }
+    deviation = sqrt(deviation) / K;
+    printf("%f ± %f", resistance_final,deviation);
+    return ;
+}
