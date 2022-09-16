db3542e84b10773ba98f1c50b49a93cd4096e17a
karetskiiVO <kar.vladi@yandex.ru>
2022-09-16 10:52:00 +0300

    v0.1.2

--- 1/main.cpp
+++ 1/main.cpp
@@ -3,10 +3,16 @@
 #include "getresults.h"
 #include "input.h"
 
+#include <stdio.h>
+
 int main () {
     float resistance[100];
-    int expcnt = preparation(resistance);
 
+    int expcnt = preparation(resistance);
+    int rescnt = data_select(expcnt, resistance);
+
+    float resistance_final = results(rescnt, resistance);
+    printf("%d\n", check(rescnt, resistance, resistance_final));
 
     return 0;
 }
