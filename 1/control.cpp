f9941eec9080b9d72f4a35464b58bba0c651b6a7
AnakinSkywaIker <84044672+AnakinSkywaIker@users.noreply.github.com>
2022-09-16 11:15:35 +0300

    Add files via upload

---
+++ 1/control.cpp
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include "control.h"
+
+int check (int K, float *resistance, float resistance_final)
+{
+    double epsilon = 1e-4;
+    double av_devation = 0;
+
+    for (int i = 0; i < K; i++)
+    {
+        av_devation += (resistance[i] - resistance_final);
+    }
+
+    av_devation /= K;
+    printf("Average deviation = %g\n", av_devation);
+
+    if (fabs (av_devation) < epsilon)
+    {
+        return 1;
+    }
+    return 0;
+}
