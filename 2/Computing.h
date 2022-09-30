e7c1eee85672ab963d2deefa9b7e28ef46c2f355
Radamoviches <55924626+Radamoviches@users.noreply.github.com>
2022-09-30 09:39:14 +0300

    Update Computing.h

---
+++ 2/Computing.h
@@ -0,0 +1,11 @@
+#ifndef COMPUTING
+#define COMPUTING
+
+double nonlinear_equation(double *radioctivity, double *time, int N, double precision);
+
+double model(double *radioctivity, double *time, int N, double point);
+
+double linear_equation(double *radioctivity, double *time, int N);
+
+
+#endif //Computing.h
