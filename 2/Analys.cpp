21162f5268115c58b9a2f55dc8bfdd4e204cc8aa
AnakinSkywaIker <Polydima8@gmail.com>
2022-09-30 11:21:21 +0300

    Update Analys.cpp

--- 2/Analys.cpp
+++ 2/Analys.cpp
@@ -1,10 +1,5 @@
 #include "Analys.h"
 
-int main (void)
-{
-    return 0;
-}
-
 double dev_exp(double *radioactivity, double *time, int M, double decay_time)
 {
     double sum_residuals1 = 0;
@@ -19,13 +14,13 @@ double dev_exp(double *radioactivity, double *time, int M, double decay_time)
     return deviation1;
 }
 
-double dev_linear(double *radioaktivity, double *time, int M, double decay_rate)
+double dev_linear(double *radioactivity, double *time, int M, double decay_rate)
 {
     double sum_residuals2 = 0;
 
     for (int i = 0; i < M; i++)
     {
-        sum_residuals2 += pow (radioaktivity[i] - (1 - (time[i] / decay_rate)), 2);
+        sum_residuals2 += pow (radioactivity[i] - (1 - (time[i] / decay_rate)), 2);
     }
 
     double deviation2 = sqrt (sum_residuals2) / M;
@@ -33,13 +28,31 @@ double dev_linear(double *radioaktivity, double *time, int M, double decay_rate)
     return deviation2;
 }
 
-double precision_analysis (double *radioaktivity, double *time, int N, double decay_time, double decay_rate)
+double precision_analysis (double *radioactivity, double *time, int N)
 {
+
     double time_differences = 0;
-    for (int M = 1; M < N; N++)
+    for (int M = 10; M < N; N++)
     {
-        double deviation1 = dev_exp (radioaktivity, time, M, decay_time);
-        double deviation2 = dev_linear (radioaktivity, time, M, decay_rate);
+        double decay_rate = 0;
+        double nom = 0;
+        double denom = 0;
+        for (int i = 0; i < M; i++)
+        {
+            nom  += pow (time[i], 2);
+            denom += time[i] * (1 - radioactivity[i]);
+        }
+        decay_rate = nom / denom;
+
+        double decay_time = 0;
+        for (int i = 0; i < M; i++)
+        {
+            decay_time = time[i] / - log (radioactivity[i]);
+        }
+        decay_time /= M;
+
+        double deviation1 = dev_exp (radioactivity, time, M, decay_time);
+        double deviation2 = dev_linear (radioactivity, time, M, decay_rate);
 
         if (deviation2 > deviation1 * 2)
         {
