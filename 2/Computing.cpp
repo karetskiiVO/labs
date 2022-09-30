1bb67b9d7593aa94d3f60612101e289ae4e803a3
Radamoviches <55924626+Radamoviches@users.noreply.github.com>
2022-09-30 12:13:20 +0300

    Update Computing.cpp

--- 2/Computing.cpp
+++ 2/Computing.cpp
@@ -27,9 +27,10 @@ double nonlinear_equation(double *radioactivity,double *time , int N, double pre
     double interval1 = 0 , interval2 = 0;
     scanf("%lf %lf" , &interval1 , &interval2);
 
-    double f1=func_nonlin(radioactivity , time , interval1 , N);
-    double middle = (interval1 + interval2) / 2.0;
-    double f2=func_nonlin(radioactivity , time , middle , N);
+    double f1 = func_nonlin(radioactivity , time , interval1 , N);
+    double middle = (interval1 + interval2) / 2;
+    double f2 = func_nonlin(radioactivity , time , middle , N);
+
     if (f1 * f2 <= 0)
     {
         interval2 = middle;
@@ -41,13 +42,20 @@ double nonlinear_equation(double *radioactivity,double *time , int N, double pre
 
     while (fabs(interval2 - interval1) > precision)
     {
-    middle = (interval1 + interval2) / 2.0;
-    f1=func_nonlin(radioactivity , time , middle , N);
-    if (fabs(f1) <= 0.001)
+
+    middle = (interval1 + interval2) / 2;
+    f1 = func_nonlin(radioactivity , time , middle , N);
+    f2 = func_nonlin(radioactivity , time, interval1 , N);
+
+    if(f1 * f2 < 0)
     {
-            return middle;
+        interval2 = middle;
     }
+    else{
+        interval1 = middle;
     }
+
+    return middle;
 }
 double model(double *radioactivity , double *time , int N, double point)
 {
