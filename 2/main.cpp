7b8482da6e3e8db0daa1ef1f776ec5af051d4b30
Radamoviches <55924626+Radamoviches@users.noreply.github.com>
2022-09-30 11:38:26 +0300

    Update main.cpp

--- 2/main.cpp
+++ 2/main.cpp
@@ -19,18 +19,9 @@ int main () {
 
     int num_data = experiment(radiactivity, time, starttime, endtime, step);
     add_noise(radiactivity, num_data);
-    
+
     double decay_time = nonlinear_equation (radiactivity, time, num_data, precision);
     double decay_rate = linear_equation(radiactivity,time,num_data); 
-
-<<<<<<< HEAD
-    double time_differences = precision_analysis (radiactivity, time, 100);
-=======
-    double time_differences = precision_analysis (radiactivity, time, num_data);
-
-
->>>>>>> a60d4c59da4a27a54960c8bf3f535f53c9b1aff8
-
 
     return 0;
 }
