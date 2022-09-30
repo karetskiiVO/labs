22b8dbf11fbfe44c1622afd91da7479900a0ca3c
AnakinSkywaIker <Polydima8@gmail.com>
2022-09-30 10:01:47 +0300

    Update Analys.h

---
+++ 2/Analys.h
@@ -0,0 +1,10 @@
+#ifndef ANALYS
+#define ANALYS
+
+#include <math.h>
+
+double precision_analysis (double *radioactivity, double *time, int N);
+double dev_exp(double *radioactivity, double *time, int M, double decay_time);
+double dev_linear(double *radioaktivity, double *time, int M, double decay_rate);
+
+#endif // Analys.h
