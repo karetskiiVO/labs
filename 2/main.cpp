0acb502962432b8112fce6e803742f2a12f51d38
AnakinSkywaIker <Polydima8@gmail.com>
2022-09-30 11:31:56 +0300

    Update main.cpp

--- 2/main.cpp
+++ 2/main.cpp
@@ -20,7 +20,9 @@ int main () {
     int num_data = experiment(radiactivity, time, starttime, endtime, step);
     add_noise(radiactivity, num_data);
 
-    double time_differences = precision_analysis (radioactivity, time, 100);
+    double time_differences = precision_analysis (radiactivity, time, num_data);
+
+
 
 
     return 0;
