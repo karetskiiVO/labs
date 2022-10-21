ad9808f960352fcae0a8fedee448227f1ba826cf
Radamoviches <55924626+Radamoviches@users.noreply.github.com>
2022-10-21 10:01:52 +0300

    Update slope_finder.h
    push

--- 3/slope_finder.h
+++ 3/slope_finder.h
@@ -4,4 +4,6 @@
 void matrix_of_slopes(int N, struct position *ship, struct fig triangle, 
                       struct position port, float (*slopes)[4]);
 
+void danger_identification(int N,float (*slopes)[4],int *danger);
+
 #endif // slope_finder.h
