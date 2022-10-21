0a5d335146065d5dad180a68ab47b2db53a377b2
karetskiiVO <kar.vladi@yandex.ru>
2022-10-21 11:12:31 +0300

    Update main.c

--- 3/main.c
+++ 3/main.c
@@ -1,9 +1,12 @@
 #include "input.h"
+#include "specialtypes.h"
 #include "slope_finder.h"
 #include "danger_identification.h"
 
 #define SHIPS_NUMBER 100
 
 int main () {
-    struct position ships[SHIPS_NUMBER] = {0};
+    position ships[SHIPS_NUMBER] = {0};
+    int couner = 0;
+    input_ship()
 }
