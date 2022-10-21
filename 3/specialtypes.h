515b26c7ff6a46cdad6f95109d49d12d1d5aad90
Daniil Knyazev <dxknyazev@gmail.com>
2022-10-21 11:38:30 +0300

    new types

--- 3/specialtypes.h
+++ 3/specialtypes.h
@@ -2,9 +2,20 @@
 #define TYPES
 
 typedef struct {
+  int degree;
+  int minutes;
+  int second;
+} coordinates;
 
-    
+typedef struct {
+  coordinates lat;
+  coordinates longitude;
 } position;
 
+typedef struct {
+  position apex1;
+  position apex2;
+  position apex3;
+} fig;
 
 #endif
