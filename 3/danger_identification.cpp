4395c477b892c9d6da2dbc620d6b7e174f38a1ca
Radamoviches <55924626+Radamoviches@users.noreply.github.com>
2022-10-21 10:07:37 +0300

    push

---
+++ 3/danger_identification.cpp
@@ -0,0 +1,14 @@
+
+void danger_identification(int N,float (*slopes)[4],int *danger)
+{
+    float max[5];
+    for (int i = 0;i < N;i++)
+    {
+
+    }
+}
+
+static void max(float *mas)
+{
+
+}
--- 3/slope_finder.h
+++ 3/slope_finder.h
@@ -4,6 +4,4 @@
 void matrix_of_slopes(int N, struct position *ship, struct fig triangle, 
                       struct position port, float (*slopes)[4]);
 
-void danger_identification(int N,float (*slopes)[4],int *danger);
-
 #endif // slope_finder.h
