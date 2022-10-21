0e109a598e0019533cfbe62a5cc0ddbe52e916a1
Radamoviches <55924626+Radamoviches@users.noreply.github.com>
2022-10-21 10:49:23 +0300

    Update danger_identification.cpp

--- 3/danger_identification.cpp
+++ 3/danger_identification.cpp
@@ -4,8 +4,9 @@ void danger_identification(int N,float (*slopes)[4],int *danger)
     for (int i = 0;i < N;i++)
     {
         float nums[5] = {slopes[i][0],slopes[i][1],slopes[i][2]};
-        float max1 = max(3,nums);
-        float max3 = max(1,nums);
+        bubbleSort(nums, 3);
+        float max1 = nums[2];
+        float max3 = nums[0];
 
         if (max1 <= slopes[i][3] <= max3)
         {
@@ -27,23 +28,13 @@ static void bubbleSort(float arr[], int n)
         {
             if (arr[j] > arr[j + 1])
             {
-                swap(&arr[j], &arr[j + 1]);
+                float temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
 }
 
-static void swap(float* xp, float* yp)
-{
-    float temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-static float max(int size,float arr[])
-{
-    bubbleSort(arr, size);
-    return arr[size-1];
-}
 
 
