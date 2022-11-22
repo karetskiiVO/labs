fe7b8d93daa6e48d9fe0b72baef104276bb50785
Radamoviches <55924626+Radamoviches@users.noreply.github.com>
2022-11-22 19:52:29 +0300

    .

--- 4/decryption.cpp
+++ 4/decryption.cpp
@@ -9,27 +9,40 @@ void decryption(int N,
                 balance* total_balance,
                 currency* val)
 {
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; ++i)
     {
         (bank_new + i)->identification_number = (bank_ciphr + i)->identification_number - (cipher + i)->ind_num;
         (bank_new + i)->account = (bank_ciphr + i)->account - (cipher + i)->key_1;
         (bank_new + i)->amount = (bank_ciphr + i)->amount - (cipher + i)->key_2;
         (bank_new + i)->code = (bank_ciphr + i)->code - (cipher + i)->key_3;
 
-        for (int j = 0; j < 5; j++)
+        if((bank_new + i)->code == (val + 0)->currency_code)
         {
-            int tmp = (val + j)->currency_code;
-            (total_balance + i)->currency_cipher[j] += (tmp * (bank_new + i)->amount); 
+            total_balance->currency_cipher[0] += (bank_new + i)->amount;
+        }
+        else if((bank_new + i)->code == (val + 1)->currency_code)
+        {
+            total_balance->currency_cipher[1] += (bank_new + i)->amount;
+        }
+        else if((bank_new + i)->code == (val + 2)->currency_code)
+        {
+            total_balance->currency_cipher[2] += (bank_new + i)->amount;
+        }
+        else if((bank_new + i)->code == (val + 3)->currency_code)
+        {
+            total_balance->currency_cipher[3] += (bank_new + i)->amount;
+        }
+        else 
+        {
+            total_balance->currency_cipher[4] += (bank_new + i)->amount;
         }
     }
 
-    for (int i = 0; i < N; i++)
+    for (int j = 0; j < 5; ++j)
     {
-        for (int j = 0; j < 5; j++)
-        {
-            printf("%d ", (total_balance + i)->currency_cipher[j]);
-        }
-        printf("\n");
+        printf("Cur_name- %s\tLetter_name- %s\tCode- %d\tStart_amount- %d\tCipher_amount- %d",
+        (val + j)->currency_name, (val + j)->leter_code, (val + j)->currency_code, 
+         total_balance->currency_start[j], total_balance->currency_cipher[j]);
     }
 
 }
