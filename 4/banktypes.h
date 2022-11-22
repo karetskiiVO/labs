494eff61cbbc403a6e080ea9a0a9377a8b50b521
karetskiiVO <kar.vladi@yandex.ru>
2022-11-22 17:24:03 +0300

    Update banktypes.h

--- 4/banktypes.h
+++ 4/banktypes.h
@@ -9,9 +9,25 @@ enum CURENCY {
 };
 
 
+typedef struct {
+    char currency_name[10];
+    char leter_code[4];
+    int currency_code;
+}  currency;
 
+typedef struct {
+ long long int identification_number;
+ int account;
+ int amount;
+ int code;
+} client;
 
-
+typedef struct {
+    int ind_num;
+    int key_1;
+    int key_2;
+    int key_3;
+} key;
 
 
 
