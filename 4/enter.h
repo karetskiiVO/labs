a36f0618e43a177c8f42d96e0634f514d91c7098
AnakinSkywaIker <Polydima8@gmail.com>
2022-11-22 18:16:09 +0300

    Create enter.h

---
+++ 4/enter.h
@@ -0,0 +1,7 @@
+#ifndef ENTER
+#define ENTER
+
+void currency_reference (struct currency *val);
+void customer_base (int N, struct client *bank,  struct currency *val, struct balance *total_balance);
+
+#endif //enter.h
